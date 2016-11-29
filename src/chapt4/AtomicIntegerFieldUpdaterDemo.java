14e076226f5e576d862fc0abf74fcc671c5ddcd2
RedColinar <1126525692@qq.com>
2016-11-29 23:14:10 +0800

    把普通变量 实现 原子操作 AtomicIntegerFieldUpdater

---
+++ src/chapt4/AtomicIntegerFieldUpdaterDemo.java
@@ -0,0 +1,33 @@
+package chapt4;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+//把普通变量 实现 原子操作 AtomicIntegerFieldUpdater
+public class AtomicIntegerFieldUpdaterDemo {
+	public static class Candidate{
+		int id;
+		volatile int  score;
+	}
+	public final  static AtomicIntegerFieldUpdater<Candidate> scoreUpdater
+		=AtomicIntegerFieldUpdater
+			.newUpdater(Candidate.class, "score");
+	//检查Updater是否正确
+	public static AtomicInteger allScore = new AtomicInteger(0);
+	
+	public static void main(String[] args) throws InterruptedException {
+		final Candidate stu = new Candidate();
+		Thread[] t= new Thread[1000];
+		for(int i=0;i<1000;i++){
+			t[i]=new Thread(()->{
+				if(Math.random()>0.4){
+					scoreUpdater.incrementAndGet(stu);
+					allScore.incrementAndGet();
+				}
+			});
+			t[i].start();
+		}
+		for(int i =0;i < 1000;i++){t[i].join();}
+		System.out.println("score="+stu.score);
+		System.out.println("allScore="+allScore);
+	}
+}
