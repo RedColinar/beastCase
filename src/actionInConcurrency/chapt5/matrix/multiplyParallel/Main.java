9ea5a45634cc49e86a48a0247d45fbb2bf7c778f
RedColinar <1126525692@qq.com>
2016-12-12 18:03:59 +0800

    并行的矩阵运算

---
+++ src/actionInConcurrency/chapt5/matrix/multiplyParallel/Main.java
@@ -0,0 +1,9 @@
+package actionInConcurrency.chapt5.matrix.multiplyParallel;
+
+public class Main {
+	//粒度
+	public static final int granularity = 3; 
+	public static void main(String[] args) {
+		
+	}
+}
